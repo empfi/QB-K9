5a801cdc630b70ca4806a96bdc50041c055bc742
empfi <empf246@gmail.com>
2025-05-16 16:01:33 +0200

    Delete shared directory

--- shared/sh_util.lua
+++
@@ -1,5 +0,0 @@
--- Shared utility functions
-function RelationshipBetweenGroups(group1, group2, relationship)
-    SetRelationshipBetweenGroups(relationship, GetHashKey(group1), GetHashKey(group2))
-    SetRelationshipBetweenGroups(relationship, GetHashKey(group2), GetHashKey(group1))
-end
